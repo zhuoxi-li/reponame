997dd215a6a383f6d6024b92047fd4c31c1c93fb
BT_C09_10 <mobileappdev@bths.edu>
2026-04-20 09:21:47 -0400

    Texas Red

---
+++ gittest/OtherView.swift
@@ -0,0 +1,21 @@
+//
+//  OtherView.swift
+//  gittest
+//
+//  Created by Student on 4/20/26.
+//
+
+import SwiftUI
+
+struct OtherView: View {
+    var body: some View {
+        Text("In this town, there lived an outlaw")
+        Text("by the name of Texas Red.")
+        Text("Many men had tried to take him,")
+        Text("And those many men were dead.")
+    }
+}
+
+#Preview {
+    OtherView()
+}
