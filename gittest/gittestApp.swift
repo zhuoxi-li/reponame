6c84ef047a7c069be8adab76011edb99de0f7862
BT_C09_10 <mobileappdev@bths.edu>
2026-04-20 09:16:40 -0400

    Initial Commit

---
+++ gittest/gittestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gittestApp.swift
+//  gittest
+//
+//  Created by Student on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct gittestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
