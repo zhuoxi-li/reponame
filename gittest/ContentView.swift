c6bed2a5db6012aef672000945b56c23e4e18225
BT_C09_10 <mobileappdev@bths.edu>
2026-04-24 09:09:01 -0400

    was an arizona ranger, wont be too long in town.

--- gittest/ContentView.swift
+++ gittest/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Text("Arizona Ranger:")
+                .font(.title)
             Image(systemName: "star.fill")
                 .imageScale(.large)
                 .foregroundStyle(.yellow)
